5af57acc0da191feeafe306f3990759ec2b3211d
A-ijishakin <ayodeji.ijishakin.21@ucl.ac.uk>
2024-05-14 23:12:43 +0100

    quicktime

--- vae_quant.py
+++ vae_quant.py
@@ -368,7 +368,7 @@ def main():
     parser.add_argument('-n', '--num-epochs', default=50, type=int, help='number of training epochs')
     parser.add_argument('-b', '--batch-size', default=2048, type=int, help='batch size')
     parser.add_argument('-l', '--learning-rate', default=1e-3, type=float, help='learning rate')
-    parser.add_argument('-z', '--latent-dim', default=10, type=int, help='size of latent dimension')
+    parser.add_argument('-z', '--latent-dim', default=512, type=int, help='size of latent dimension')
     parser.add_argument('--beta', default=1, type=float, help='ELBO penalty term')
     parser.add_argument('--tcvae', action='store_true')
     parser.add_argument('--exclude-mutinfo', action='store_true')
