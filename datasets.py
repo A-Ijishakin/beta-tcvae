a59c968de806e4d970004b9fbd44ce49079b19d4
A-ijishakin <ayodeji.ijishakin.21@ucl.ac.uk>
2024-05-15 22:29:22 +0100

    non-block

--- datasets.py
+++ datasets.py
@@ -9,51 +9,51 @@ import pandas as pd
 
 class CelebA_Dataset(torch.utils.data.Dataset):
     def __init__(self, mode=0, classification=False, ffhq=''):
-        # #filter for those in the training set
-        # self.datums = pd.read_csv('celeba.csv')
-        # self.datums = self.datums[self.datums['set'] == mode]  
-        # self.ffhq = ffhq 
-        # #instantiate the base directory 
-        # self.base = '../img_align_celeba' 
-        # self.transform = tfs.Compose([
-        #     tfs.ToTensor(), 
-        #     tfs.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5), inplace=True)
-        # ])
+        #filter for those in the training set
+        self.datums = pd.read_csv('celeba.csv')
+        self.datums = self.datums[self.datums['set'] == mode]  
+        self.ffhq = ffhq 
+        #instantiate the base directory 
+        self.base = '../img_align_celeba' 
+        self.transform = tfs.Compose([
+            tfs.ToTensor(), 
+            tfs.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5), inplace=True)
+        ])
         
         
         self.data = torch.randn(1, 3, 512, 512)
         
     def __len__(self): 
-        # return len(self.datums) - 1 
-        return 3000 
+        return len(self.datums) - 1 
+
     
     def __getitem__(self, idx):
-        # path = '{}/{}'.format(self.base, 
-        #         self.datums.iloc[idx]['id']) 
+        path = '{}/{}'.format(self.base, 
+                self.datums.iloc[idx]['id']) 
         
-        # x = Image.open(path) 
+        x = Image.open(path) 
         
-        # # Crop the center of the image
-        # w, h = x.size 
-        # crop_size = min(w, h)
+        # Crop the center of the image
+        w, h = x.size 
+        crop_size = min(w, h)
 
-        # left    = (w - crop_size)/2
-        # top     = (h - crop_size)/2
-        # right   = (w + crop_size)/2
-        # bottom  = (h + crop_size)/2
+        left    = (w - crop_size)/2
+        top     = (h - crop_size)/2
+        right   = (w + crop_size)/2
+        bottom  = (h + crop_size)/2
 
-        # # Crop the center of the image
-        # x = x.crop((left, top, right, bottom))
+        # Crop the center of the image
+        x = x.crop((left, top, right, bottom))
 
-        # # resize the image
-        # x = x.resize((512, 512))      
+        # resize the image
+        x = x.resize((512, 512))      
                 
-        # if self.transform is not None:
-        #     x = self.transform(x) 
+        if self.transform is not None:
+            x = self.transform(x) 
                     
-        # labels = torch.tensor(self.datums.iloc[idx].drop(['id', 'set']).values.astype(float))
-        # return {'img': x.to(torch.float32),  
-        #         'index' : idx, 'path': path, 'labels': labels}   
+        labels = torch.tensor(self.datums.iloc[idx].drop(['id', 'set']).values.astype(float))
+        return {'img': x.to(torch.float32),  
+                'index' : idx, 'path': path, 'labels': labels}   
             
     
         return {'img': self.data, 'index' : idx} 
